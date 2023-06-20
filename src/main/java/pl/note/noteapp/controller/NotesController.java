ff2941f15a57efc4d3663b8b9473d15a1b43c717
TomaszSiarkiewicz <TomaszSiarkiewicz@gmial.com>
2023-06-20 10:44:39 +0200

    deploy fix

--- src/main/java/pl/note/noteapp/controller/NotesController.java
+++ src/main/java/pl/note/noteapp/controller/NotesController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.note.noteapp.dtos.*;
 import pl.note.noteapp.notes.NotesFacade;
-deploy fiximport org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
